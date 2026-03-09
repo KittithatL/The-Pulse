4caaebfd10b4fbe15f751467367bc0a0e15cd2d5
saranyaran doksanthia <165564138+misssryr@users.noreply.github.com>
2026-03-10 01:45:13 +0700

    finish authentication system (login, register, 2FA, reset password)

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const authenticate = require('../middleware/authenticate');
 
-// Public routes
+// test route
+router.get('/test', (req, res) => {
+  res.send("API working");
+});
+
+// register
 router.post('/register', authController.register);
-router.post('/login', authController.login);
 
-// Protected routes
-router.get('/me', authenticate, authController.getCurrentUser);
+// login
+router.post('/login', authController.login);
 
 module.exports = router;
